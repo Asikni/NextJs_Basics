f1804ac6e717fb5aff276251ad610ef3815b77c2
Imran <mohdimranshountoo@gmail.com>
2024-04-28 12:57:05 +0530

    code updated

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,4 +1,26 @@
 //[productId] is the dynamic routing
+import { Metadata } from "next";
+
+type Props ={
+  params : {
+    productId:string;
+  }
+}
+
+export const generateMetadata = async ({
+  params,
+}:Props): Promise<Metadata> => {
+  const title = `i-phone ${params.productId}`
+
+  // const title = await new Promise((resolve)=>{   //why like this??
+  //   setTimeout(() => {
+  //     resolve(`i-phone ${params.productId}`)
+  //   }, 100);
+  // });
+  return{
+    title:`Product ${title}`
+  }
+}
 
 export default function ProductDetails({
   params,
