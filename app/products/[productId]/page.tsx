43d2ef8555a1cb9fdad7349121db1b5ba4b30714
Imran <mohdimranshountoo@gmail.com>
2024-05-01 21:33:06 +0530

    code updated

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -2,22 +2,22 @@
 import { Metadata } from "next";
 import { clothes } from "@/app/utils/const/clothes";
 
+type Props = {
+  params: {
+    //params provided by the next
+    productId: string;
+  };
+};
 
-type Props ={
-  params : {  //params provided by the next
-    productId:string;
-  }
-}
-
-export const generateMetadata = ({params}:Props): Metadata =>{
-  return{
-    title: `i-phone ${params.productId}`
-}}
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `i-phone ${params.productId}`,
+  };
+};
 
 // export const generateMetadata = async ({
 //   params,
 // }:Props): Promise<Metadata> => {
- 
 
 //   const title = await new Promise((resolve)=>{   //why like this??
 //     setTimeout(() => {
@@ -29,15 +29,33 @@ export const generateMetadata = ({params}:Props): Metadata =>{
 //   }
 // }
 
-
-console.log(clothes)
 export default function ProductDetails({
   params,
 }: {
-  params: { productId: number };
+  params: { productId: string };
 }) {
-  return <h1> Details about the product {params.productId}
-  <p>{params.productId}</p>
- 
-  </h1>;
+  return (
+    <h1>
+      {" "}
+      Details about the product {params.productId}
+      {typeof(params.productId)}
+      <ul>
+        {clothes.map((cloth, index) =>
+        
+          index === parseInt(params.productId) ? (
+            <li key={index}>
+              <div>
+                <p>Type: {cloth.type}</p>
+                <p>Color: {cloth.color}</p>
+                <p>Size: {cloth.size}</p>
+                <p>Brand: {cloth.brand}</p>
+                <p>Price: {cloth.price}</p>
+                
+              </div>
+            </li>
+          ) : null
+        )}
+      </ul>
+    </h1>
+  );
 }
