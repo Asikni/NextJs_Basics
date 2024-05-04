a434289b9fd2ebfdca5a46ef114348738f200fbd
Imran <mohdimranshountoo@gmail.com>
2024-05-04 18:38:55 +0530

    code updated

--- app/b_profile/api/route.ts
+++ app/b_profile/api/route.ts
@@ -6,14 +6,18 @@ import { headers } from "next/headers";
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers); //first method
   const headerList = headers(); //next provides headers on its own
-
+  const theme = request.cookies.get("theme")
+ 
   console.log(requestHeaders.get("Authorization"));
   console.log(headerList.get("Authorization"));
   console.log(headerList.get("Accept"));
+  console.log(theme)
+
 
   return new Response("<h1> profile </h1>", {
     headers: {
       "Content-Type": "text/html",
+      'Set-Cookie':'theme-dark'
     },
   });
 }
